aed77378d3efb330a45c5993830bc9f1acb239db
Shiva Kumar Jakkani <shivakumarjakkani@gmail.com>
2015-12-25 18:58:11 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+         <meta charset="utf-8">
+         <link rel="stylesheet" type="text/css" href="css/main.css">
+         <title>myBox</title>
+    </head>
+    <body>
+		<header class="logo">
+			<h1>myBox</h1>
+			<h5>MY DATA - MY RUELS</h5>
+		</header>
+    <body>
+</html>
