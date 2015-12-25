7f66c136c799f905bbee65c2a4ca76b60b295e31
Shiva Kumar Jakkani <shivakumarjakkani@gmail.com>
2015-12-25 22:16:57 +0530

    Added a gitignore file

--- index.php
+++ index.php
@@ -10,5 +10,17 @@
 			<h1>myBox</h1>
 			<h5>MY DATA - MY RUELS</h5>
 		</header>
+		
+		<!-- Login form -->
+		<section id="login-form">
+		    <form action="">
+		        <div id="fields">
+			        <p> Welcome to myBox Loign </p>
+		    		<input type="text" value="USER NAME" name="uname">
+		    		<input type="password" value="PASS WORD" name="pword">
+		    		<input type="submit" value="VALIDATE" name="login">
+	    		</div>
+		    </form>
+		</section>
     <body>
 </html>
